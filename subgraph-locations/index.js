0706088238c8aa57eac592d6cd1beecd759bf1ab
markkong30 <markkong30@gmail.com>
2022-11-16 17:28:47 +0000

    set up

--- subgraph-locations/index.js
+++ subgraph-locations/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const { buildSubgraphSchema } = require('@apollo/subgraph');
 
 const { readFileSync } = require('fs');
 const gql = require('graphql-tag');
@@ -9,27 +10,27 @@ const resolvers = require('./resolvers');
 const LocationsAPI = require('./datasources/LocationsApi');
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs, resolvers });
+	const server = new ApolloServer({ schema: buildSubgraphSchema(typeDefs, resolvers) });
 
-  const port = 4001;
-  const subgraphName = 'locations';
+	const port = 4001;
+	const subgraphName = 'locations';
 
-  try {
-    const { url } = await startStandaloneServer(server, {
-      context: async () => {
-        return {
-          dataSources: {
-            locationsAPI: new LocationsAPI(),
-          },
-        };
-      },
-      listen: { port },
-    });
+	try {
+		const { url } = await startStandaloneServer(server, {
+			context: async () => {
+				return {
+					dataSources: {
+						locationsAPI: new LocationsAPI()
+					}
+				};
+			},
+			listen: { port }
+		});
 
-    console.log(`🚀 Subgraph ${subgraphName} running at ${url}`);
-  } catch (err) {
-    console.error(err);
-  }
+		console.log(`🚀 Subgraph ${subgraphName} running at ${url}`);
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 startApolloServer();
--- subgraph-reviews/index.js
+++ subgraph-reviews/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const { buildSubgraphSchema } = require('@apollo/subgraph');
 
 const { readFileSync } = require('fs');
 const gql = require('graphql-tag');
@@ -9,27 +10,27 @@ const resolvers = require('./resolvers');
 const ReviewsAPI = require('./datasources/ReviewsApi');
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs, resolvers });
+	const server = new ApolloServer({ schema: buildSubgraphSchema(typeDefs, resolvers) });
 
-  const port = 4002;
-  const subgraphName = 'reviews';
+	const port = 4002;
+	const subgraphName = 'reviews';
 
-  try {
-    const { url } = await startStandaloneServer(server, {
-      context: async () => {
-        return {
-          dataSources: {
-            reviewsAPI: new ReviewsAPI(),
-          },
-        };
-      },
-      listen: { port },
-    });
+	try {
+		const { url } = await startStandaloneServer(server, {
+			context: async () => {
+				return {
+					dataSources: {
+						reviewsAPI: new ReviewsAPI()
+					}
+				};
+			},
+			listen: { port }
+		});
 
-    console.log(`🚀 Subgraph ${subgraphName} running at ${url}`);
-  } catch (err) {
-    console.error(err);
-  }
+		console.log(`🚀 Subgraph ${subgraphName} running at ${url}`);
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 startApolloServer();
